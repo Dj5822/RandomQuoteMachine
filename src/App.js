de131cd8c82a1f332fa72a271d13b83ea374cad6
Dylan Jung <>
2020-12-11 22:26:44 +1300

    Added basic components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import './QuoteBoxStyle.css';
+import React from 'react';
+
+class QuoteMachine extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quote: "",
+      author: ""
+    }
+  }
+
+  render(){
+    return(
+      <div id="quote-box">
+        <p id="text">This is a quote.</p>
+        <p id="author">Author: </p>
+        <button id="new-quote">New Quote</button>
+        <a id="tweet-quote">Tweet</a>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <QuoteMachine />
     </div>
   );
 }
